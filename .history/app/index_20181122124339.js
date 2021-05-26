001b4b84e1fed6ea035366d7bb1e13e5bc3aafb7
Loïc <loicbere@gmail.com>
2021-05-26 16:15:42 +0200

    Delete .history directory

--- .history/app/index_20181122124339.js
+++
@@ -1,6 +0,0 @@
-import './index.css';
-
-import App from './scripts/SpherePartition';
-import OBJLoader from '../node_modules/three/examples/js/loaders/OBJLoader';
-
-window.app = new App();
--- .history/app/index_20210526144526.js
+++
@@ -1,6 +0,0 @@
-import './index.css';
-
-import App from './scripts/ToonMaterial';
-import OBJLoader from '../node_modules/three/examples/js/loaders/OBJLoader';
-
-window.app = new App();
